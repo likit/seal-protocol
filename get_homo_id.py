2863178025044544200a934dcf210e258ac7f4b1
Likit Preeyanon <preeyano@msu.edu>
2014-04-16 16:02:13 -0400

    update the code

--- get_homo_id.py
+++ get_homo_id.py
@@ -2,8 +2,15 @@ import csv
 import sys
 
 with open(sys.argv[1]) as fp:
-    reader = csv.reader(fp, dialect="csv")
+    reader = csv.reader(fp)
+    writer = csv.writer(sys.stdout, dialect="excel-tab")
     for line in reader:
-        print line
-        break
+        if line[0] == "sequence name":
+            continue
+        elif line[5] == '':
+            continue
+        else:
+            writer.writerow(line[0], line[1],
+                    line[2], line[3], line[5])
+            break
 

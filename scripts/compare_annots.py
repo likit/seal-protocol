ab1df86bca662bd5c77dd10965a5fbdf4191b522
Likit Preeyanon <preeyano@msu.edu>
2014-06-17 13:27:38 -0400

    updated code

--- scripts/compare_annots.py
+++ scripts/compare_annots.py
@@ -12,50 +12,55 @@ dog = read_csv('mirounga-dog.fa.annot.csv', header=0)
 
 # removed transcripts with no orthologs
 dog_ortho = dog[dog.ortholog.notnull()]
+
+# removed transcripts with no homologs
 dog_homo = dog[dog.homolog.notnull()]
 
+# removed transcripts with no orthologs
 mus_ortho = mus[mus.ortholog.notnull()]
+
+# removed transcripts with no homologs
 mus_homo = mus[mus.homolog.notnull()]
 
-annotdog = set(dog_ortho['unique ID'].tolist())
-annotdog.update(set(dog_homo['unique ID'].tolist()))
+annot_dog = set(dog_ortho['unique ID'].tolist())
+annot_dog.update(set(dog_homo['unique ID'].tolist()))
 
-annotmus = set(mus_ortho['unique ID'].tolist())
-annotmus.update(set(mus_homo['unique ID'].tolist()))
+annot_mus = set(mus_ortho['unique ID'].tolist())
+annot_mus.update(set(mus_homo['unique ID'].tolist()))
 
-dogonly = set(annotdog).difference(set(annotmus))
+dog_only = annot_dog.difference(annot_mus)
 
 # print the number of unique transcripts
-print 'unique transcript = ', len(dogonly)
+print 'unique transcript = ', len(dog_only)
 
 idx = []
 
 for row in dog.iterrows():
     features = row[1]
-    if features['unique ID'] in dogonly:
+    if features['unique ID'] in dog_only:
         idx.append(row[0])
 
-dogonly_table = dog.ix[idx]
+dog_only_table = dog.ix[idx]
 
-dogonly_table.to_csv('dog_annots_only_id.txt')
+dog_only_table.to_csv('dog_annots_only_id.txt')
 
-annotdog = set(dog_ortho['Transcript family'].tolist())
-annotdog.update(set(dog_homo['Transcript family'].tolist()))
-annotmus = set(mus_ortho['Transcript family'].tolist())
-annotmus.update(set(mus_homo['Transcript family'].tolist()))
+annot_dog = set(dog_ortho['Transcript family'].tolist())
+annot_dog.update(set(dog_homo['Transcript family'].tolist()))
+annot_mus = set(mus_ortho['Transcript family'].tolist())
+annot_mus.update(set(mus_homo['Transcript family'].tolist()))
 
-dogonly = set(annotdog).difference(set(annotmus))
+dog_only = set(annot_dog).difference(set(annot_mus))
 
 # print the number of unique transcripts
-print 'unique family = ', len(dogonly)
+print 'unique family = ', len(dog_only)
 
 idx = []
 
 for row in dog.iterrows():
     features = row[1]
-    if features['Transcript family'] in dogonly:
+    if features['Transcript family'] in dog_only:
         idx.append(row[0])
 
-dogonly_table = dog.ix[idx]
+dog_only_table = dog.ix[idx]
 
-dogonly_table.to_csv('dog_annots_only_family.txt')
+dog_only_table.to_csv('dog_annots_only_family.txt')
